a08281b1e51dab5e40e5f80537cd58c7ee9c1305
Piotr Starzec <piotr.starzec@krollontrack.com>
2018-02-07 15:43:54 +0100

    standard select fix

--- js/bootstrap-material-theme.js
+++ js/bootstrap-material-theme.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
+    // Buttons
     $('.btn').click(function () {
         console.log('button clicked');
     });
 
+    // Forms
     $('.form-control')
     // form-control focus
         .focus(function () {
@@ -11,13 +13,20 @@ $(document).ready(function () {
         // form-control blur
         .blur(function () {
             $(this).parent('.form-group').removeClass('is-focused');
-        })
-        // form-control filled
-        .keyup(function () {
-            if ($(this).val()) {
+            const value = $(this).val();
+            if (value && value !== '' && value.length !== 0) {
                 $(this).parent('.form-group').addClass('is-filled');
             } else {
                 $(this).parent('.form-group').removeClass('is-filled');
             }
+        })
+        // form-control filled
+        .keyup(function () {
+            // const value = $(this).val();
+            // if (value) {
+            //     $(this).parent('.form-group').addClass('is-filled');
+            // } else {
+            //     $(this).parent('.form-group').removeClass('is-filled');
+            // }
         });
 });
